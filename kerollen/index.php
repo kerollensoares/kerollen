1b94e216692926e9d7ca9f566c3bbd605d3a139f
kerollen <kerollen.soares@gmail.com>
2013-10-06 17:31:15 -0300

    Alteracoes de Estrutura

---
+++ kerollen/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html class=" js no-flexbox canvas canvastext no-webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths"><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<head>
+		<title>Kerollen Soares</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">   
+		<meta name="description" content="Kerollen Soares Solucoes">
+		<meta name="author" content="Kerollen Soares"> 
+		<script src="source/js/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="assets/js/jquery-1.7.2.min.js"><\/script>')</script>
+		<script src="source/js/jquery-ui-1.8.22.custom.min.js"></script>
+		<script src="source/js/jquery.imagesloaded.min.js"></script>
+		<script src="source/js/video.js"></script>
+		<script src="source/js/modernizr-2.5.3.min.js"></script>
+		<link href="source/css/css" rel="stylesheet" type="text/css">
+		<link href="source/css/css(1)" rel="stylesheet" type="text/css">
+		<script type="text/javascript" src="source/js/bootstrap.min.js"></script> 
+		<script src="source/js/bigvideo.js"></script> 
+		<link href="source/css/bootstrap.min.css" rel="stylesheet">
+		<link href="source/css/bootstrap-responsive.min.css" rel="stylesheet">
+		<link href="source/css/main.css" rel="stylesheet">    
+	  
+	</head>
+	<body>
+		
+		<div id="big-video-wrap">
+			<div id="big-video-vid" class="video-js vjs-default-skin vjs-playing" width="300" height="150" style="width: 1424px; height: 801px; display: block; top: -52.5px; left: 0px; background:url('imagem003.jpg') no-repeat fixed center;">
+			<!--<div id="big-video-vid" class="video-js vjs-default-skin vjs-playing" width="300" height="150" style="width: 1424px; height: 801px; display: block; top: -52.5px; left: 0px;">-->
+				<video id="big-video-vid_html5_api" class="vjs-tech" preload="auto" data-setup="{}" webkit-playsinline="" autoplay="" style="position: absolute; width: 100%;" src="image001.jpg"></video>
+			</div>
+		</div>
+
+		<div class="navbar navbar-fixed-top">
+		  <div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				<a class="brand scroller" data-section="html" href="">Soluções em Sites e Sistemas</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav pull-right">
+						<li><a href="saved_resource.htm" class="scroller" data-section="#intro">Inicio</a></li>
+						<li><a href="saved_resource.htm" class="scroller" data-section="#about">Serviços</a></li>
+						<li><a href="saved_resource.htm" class="scroller" data-section="#team">Sobre Kerollen</a></li>
+						<li><a href="saved_resource.htm" class="scroller" data-section="#contact">Contato</a></li>
+						<li class="dropdown">
+							<a href="saved_resource.htm" class="dropdown-toggle" data-toggle="dropdown">
+								<img src="config.png"></img>
+								<!--<b class="caret"></b>-->
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="saved_resource.htm">Área restrita</a></li>
+								<li><a href="http://oficinadecodigos.blogspot.com.br/" TARGET='_blank'>Oficina de Códigos</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		  </div>
+		</div>
+		
+		<div class="main-container">
+			<div class="container-fluid" id="intro">	
+				<div class="container">	
+					<div class="row span5 center">
+						<h1>Kerollen Soares</h1>
+						<p>Informações.</p>
+						<a href="saved_resource.htm" class="btn btn-xlarge btn-purple">Entrar</a>
+					</div>	
+				</div>
+			</div>
+		
+			<div class="container-fluid" id="about">
+				<div class="container light">
+					<h1>Nossos serviços oferecem</h1>
+					<ul class="thumbnails">
+					  <li class="span4">
+						<div class="thumbnail">
+						  <img alt="" src="icon1.png">
+						  <h3>Organização</h3>
+						  <p></p>
+						</div>
+					  </li>
+					
+					  <li class="span4">
+						<div class="thumbnail">
+						  <img alt="" src="icon2.png">
+						  <h3>Qualidade</h3>
+						  <p></p>
+						</div>
+					  </li>
+					  
+					  <li class="span4">
+						<div class="thumbnail">
+						   <img alt="" src="icon3.png">
+						  <h3>Criatividade e Interação</h3>
+						  <p></p>
+						</div>
+					  </li>
+					</ul>
+				</div>
+			</div>
+			
+			<div class="container-fluid" id="team">
+				<div class="container dark">
+					<h1>Conheça nosso time</h1>
+					<ul class="thumbnails">
+					  <li class="span3">
+						<div class="thumbnail">
+						  <img alt="" src="fotoPerfil.png">
+						  <h3>Kerollen Soares</h3>
+						  <p>Profissional em Desenvolvimento WEB a 6 anos no mercado diretamente com sistemas empresariais.</p>
+						  <div class="social">
+							  <a href="http://br.linkedin.com/in/kerollensoares" target="_blank"><img alt="" src="linkedin.png"></a>
+							  <a href="http://www.twitter.com/kerollensoares" target="_blank"><img alt="" src="twitter.png"></a>
+							  <a href="http://www.facebook/kerollensoares" target="_blank"><img alt="" src="facebook.png"></a>
+							  <a href="mailto:kerollen.soares@gmail.com" ><img alt="" src="email.png"></a>
+						  </div>
+						</div>
+					  </li>
+					  
+					  <li class="span3">
+						<div class="thumbnail">
+						<br /><br /><br /><br /><br />
+						  <p>Área de Atuação: Desenvolvimento em PHP, Base em JavaScritpt, Gestão e Desenvolvimento em EaD - Ferramenta Moodle, Banco de Dados mySQL, Firebird.
+							Características: determinação, persistência e pro-atividade.</p>
+						</div>
+					  </li>
+					  
+					</ul>
+				</div>
+			</div>
+			
+			<div class="container-fluid" id="contact">
+				<div class="container dark">
+					<h1>Entre em contato conosco</h1>	
+				
+					<div class="span5">			
+						<form>
+							  <div class="control-group">
+								<label class="control-label" for="inputName">Nome</label>
+								<div class="controls">
+								  <input type="text" class="span4" id="inputName" placeholder="Name">
+								</div>
+							  </div>
+							  <div class="control-group">
+								<label class="control-label" for="inputEmail">Email</label>
+								<div class="controls">
+								  <input type="text" class="span4" id="inputEmail" placeholder="Email">
+								</div>
+							  </div>
+							  <div class="control-group">
+								<label class="control-label" for="inputContact">Messagem</label>
+								<div class="controls">
+								 <textarea id="inputContact" class="span4"></textarea>
+								</div>
+							  </div>
+							  <div class="control-group">
+								<div class="controls">
+								  <button type="submit" class="btn btn-purple">Nos contate</button>
+								</div>
+							  </div>
+						</form>
+					</div>
+				
+					<div class="span4">	
+							<h2>Chat</h2>
+							
+							<p>Informações de contato.</p>
+				
+							<address>
+							  <strong>Kerollen Soares ,Inc.</strong><br>
+							  Avenida, 0000, Colety<br>
+							  Muriaé, MG 36880000<br>
+							  <abbr title="Phone">P:</abbr> (32)91246030
+							</address>
+							 
+							<address>
+							  <strong>Email</strong><br>
+							  <a href="mailto:#">kerollen.soares@gmail.com</a>
+							</address>		
+					</div>		
+				</div>
+			</div>
+		</div>
+		
+	<script>
+	$(function () {
+		$(window).scroll(function(){
+		// global scroll to top button
+			if ($(this).scrollTop() > 300) {
+				$('.scrolltop').fadeIn();
+			} else {
+				$('.scrolltop').fadeOut();
+			}        
+		});
+		
+		// scroll nav
+		$('.scroller').click(function(){
+			var section = $($(this).data("section"));
+			var top = section.offset().top-82;
+			$("html, body").animate({ scrollTop: top }, 700);
+			return false;
+		});
+
+					
+		// For the purpose of the demo this allows you to toggle the background between video and img
+		$('.toggle_video').click(function(){
+			
+		   $('#big-video-wrap').fadeToggle();
+		   return false; 
+			
+		});
+		
+		// BigVideo Setup
+		var BV = new $.BigVideo({useFlashForFirefox:false});
+		BV.init();
+		if (Modernizr.touch) {
+			// fallback image for mobile devices
+			BV.show('assets/img/bg2.jpg');
+		} else {
+			BV.show('assets/video/frontier.mp4',
+			{ambient:true, doLoop:true, altSource:'assets/video/frontier.ogv'});
+		}   
+		
+				
+	});
+	</script>
+
+		<div id="big-video-control-container" style="display: none;">
+			<div id="big-video-control">
+				<a href="saved_resource.htm" id="big-video-control-play"></a>
+				<div id="big-video-control-middle">
+					<div id="big-video-control-bar">
+						<div id="big-video-control-bound-left"></div>
+						<div id="big-video-control-progress" style="width: 19.883226801439548%;"></div>
+						<div id="big-video-control-track" class="ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all">
+							<a class="ui-slider-handle ui-state-default ui-corner-all" href="saved_resource.htm" style="left: 20.04%;"></a>
+						</div>
+						<div id="big-video-control-bound-right"></div>
+					</div>
+				</div>
+				<div id="big-video-control-timer">0:03/0:16</div>
+			</div>
+		</div>
+	</body>
+</html>
